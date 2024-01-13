6d43f9b0b22afd61d3b8746290b42aedf52eb942
AiverAiva <yaminocreeper@gmail.com>
2024-01-14 03:16:41 +0800

    refactor: code formatting

--- src/main/java/me/weikuwu/cute/config/Config.java
+++ src/main/java/me/weikuwu/cute/config/Config.java
@@ -4,87 +4,170 @@ import me.weikuwu.cute.config.settings.Property;
 
 public class Config {
     @Property(
-            name = "Scrollable Tooltips", note = "u scroll",
+            name = "Scrollable Tooltips",
+            note = "u scroll",
             type = Property.Type.BOOLEAN
     )
     public static boolean masterToggle = true;
 
     @Property(
-            name = "Horizontal Scrolling", note = "Turns horizontal scrolling on/off.", parent = "Scrollable Tooltips",
+            parent = "Scrollable Tooltips",
+            name = "Horizontal Scrolling",
+            note = "Turns horizontal scrolling on/off.",
             type = Property.Type.BOOLEAN
     )
     public static boolean horizontalScrolling = true;
 
     @Property(
-            name = "Vertical Scrolling", note = "Turns vertical scrolling on/off.", parent = "Scrollable Tooltips",
+            parent = "Scrollable Tooltips",
+            name = "Vertical Scrolling",
+            note = "Turns vertical scrolling on/off.",
             type = Property.Type.BOOLEAN
     )
     public static boolean verticalScrolling = true;
 
     @Property(
-            name = "Tooltip Zooming", note = "Turns zooming on tooltips on/off.", parent = "Scrollable Tooltips",
+            parent = "Scrollable Tooltips",
+            name = "Tooltip Zooming",
+            note = "Turns zooming on tooltips on/off.",
             type = Property.Type.BOOLEAN
     )
     public static boolean zoom = true;
 
     @Property(
-            name = "Start at the Top of Tooltips", note = "Changes tooltips to always show the top.", parent = "Scrollable Tooltips",
+            parent = "Scrollable Tooltips",
+            name = "Start at the Top of Tooltips",
+            note = "Changes tooltips to always show the top.",
             type = Property.Type.BOOLEAN
     )
     public static boolean startAtTop = true;
 
-    @Property(type = Property.Type.BOOLEAN, name = "Show Candies", note = "Show candies on pet.")
+    @Property(
+            name = "Show Candies",
+            note = "Show candies on pet.",
+            type = Property.Type.BOOLEAN
+    )
     public static boolean showCandies = false;
 
-    @Property(type = Property.Type.BOOLEAN, name = "Hide Armor")
+    @Property(
+            name = "Hide Armor",
+            type = Property.Type.BOOLEAN
+    )
     public static boolean hideArmor = false;
 
-    @Property(type = Property.Type.BOOLEAN, name = "Auto Close Secret Chests")
+    @Property(
+            name = "Auto Close Secret Chests",
+            type = Property.Type.BOOLEAN
+    )
     public static boolean closeSecretChests = false;
 
-    @Property(type = Property.Type.NUMBER, name = "Close Delay", parent = "Auto Close Secret Chests", max = 20, suffix = " ticks")
+    @Property(
+            parent = "Auto Close Secret Chests",
+            name = "Close Delay",
+            max = 20, suffix = " ticks",
+            type = Property.Type.NUMBER
+    )
     public static int closeSecretChestsDelay = 1;
 
-    @Property(type = Property.Type.BOOLEAN, name = "Ghost Pickaxe", note = "Create ghost blocks when breaking.")
+    @Property(
+            name = "Ghost Pickaxe",
+            note = "Create ghost blocks when breaking.",
+            type = Property.Type.BOOLEAN
+    )
     public static boolean ghostPickaxe = false;
 
-    @Property(type = Property.Type.BOOLEAN, name = "On Shift", parent = "Ghost Pickaxe")
+    @Property(
+            parent = "Ghost Pickaxe",
+            name = "On Shift",
+            type = Property.Type.BOOLEAN
+    )
     public static boolean ghostPickaxeOnShift = false;
 
-    @Property(type = Property.Type.BOOLEAN, name = "Only in Dungeons", parent = "Ghost Pickaxe")
+    @Property(
+            parent = "Ghost Pickaxe",
+            name = "Only in Dungeons",
+            type = Property.Type.BOOLEAN
+    )
     public static boolean ghostPickaxeOnlyInDungeon = false;
 
-    @Property(type = Property.Type.BOOLEAN, name = "Auto Fish")
+    @Property(
+            name = "Auto Fish",
+            type = Property.Type.BOOLEAN
+    )
     public static boolean autoFish = false;
 
-    @Property(type = Property.Type.NUMBER, name = "Recast Delay", parent = "Auto Fish", max = 1000, step = 50, suffix = " ms")
+    @Property(
+            parent = "Auto Fish",
+            name = "Recast Delay",
+            max = 1000, step = 50, suffix = " ms",
+            type = Property.Type.NUMBER
+    )
     public static int recastDelay = 350;
 
-    @Property(type = Property.Type.NUMBER, name = "Aim Speed", parent = "Auto Fish", max = 1000, step = 50, suffix = " ms")
+    @Property(
+            parent = "Auto Fish",
+            name = "Aim Speed",
+            max = 1000, step = 50, suffix = " ms",
+            type = Property.Type.NUMBER
+    )
     public static int aimSpeed = 250;
 
-    @Property(type = Property.Type.NUMBER, name = "Hyperion Damage", parent = "Auto Fish", max = 3000000, step = 250000)
+    @Property(
+            parent = "Auto Fish",
+            name = "Hyperion Damage",
+            max = 3000000, step = 250000,
+            type = Property.Type.NUMBER,
+    )
     public static int hypDamage = 1000000;
 
-    @Property(type = Property.Type.NUMBER, name = "Sea Creature Range", parent = "Auto Fish", max = 10, suffix = " blocks")
+    @Property(
+            parent = "Auto Fish",
+            name = "Sea Creature Range",
+            max = 10, suffix = " blocks",
+            type = Property.Type.NUMBER
+    )
     public static int scRange = 6;
 
-    @Property(type = Property.Type.CHECKBOX, name = "Instakill Sea Creatures", parent = "Auto Fish")
+    @Property(
+            parent = "Auto Fish",
+            name = "Instakill Sea Creatures",
+            type = Property.Type.CHECKBOX
+    )
     public static boolean killPrio = true;
 
-    @Property(type = Property.Type.CHECKBOX, name = "Anti AFK", parent = "Auto Fish")
+    @Property(
+            parent = "Auto Fish",
+            name = "Anti AFK",
+            type = Property.Type.CHECKBOX
+    )
     public static boolean antiAfk = true;
 
-    @Property(type = Property.Type.CHECKBOX, name = "Slug Mode", parent = "Auto Fish")
+    @Property(
+            parent = "Auto Fish",
+            type = Property.Type.CHECKBOX,
+            name = "Slug Mode"
+    )
     public static boolean slugMode = false;
 
-    @Property(type = Property.Type.CHECKBOX, name = "Sneak", parent = "Auto Fish")
+    @Property(
+            parent = "Auto Fish",
+            name = "Sneak",
+            type = Property.Type.CHECKBOX
+    )
     public static boolean sneak = false;
 
-    @Property(type = Property.Type.CHECKBOX, name = "Debug Mode", parent = "Auto Fish")
+    @Property(
+            parent = "Auto Fish",
+            name = "Debug Mode",
+            type = Property.Type.CHECKBOX
+    )
     public static boolean debugMode = false;
 
-    @Property(type = Property.Type.BOOLEAN, name = "Developer Mode", note = "weiku is cute")
+    @Property(
+            name = "Developer Mode",
+            note = "weiku is cute",
+            type = Property.Type.BOOLEAN
+    )
     public static boolean devMode = false;
 /*
     @Setting(name = "PetSwap", description = "Activates PetSwap on bobber in water")
